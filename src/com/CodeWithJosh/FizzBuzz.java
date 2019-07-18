0c4d58b1797bb7bd0b57e020678c52bdb65ebf49
peterlulu <peterlulu666@gmail.com>
2019-07-18 14:20:44 -0400

    CodeWithJosh

---
+++ src/com/CodeWithJosh/FizzBuzz.java
@@ -0,0 +1,31 @@
+package com.CodeWithJosh;
+
+import java.util.Date;
+import java.util.Scanner;
+
+
+public class FizzBuzz {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Type a number: ");
+        int number = scanner.nextInt();
+
+        if (number % 5 == 0 && number % 3 == 0)
+            System.out.println("FizzBuzz");
+        else if (number % 5 == 0)
+            System.out.println("Fizz");
+        else if (number % 3 == 0)
+            System.out.println("Buzz");
+        else
+            System.out.println(number);
+
+
+
+
+
+
+
+
+    }
+}
